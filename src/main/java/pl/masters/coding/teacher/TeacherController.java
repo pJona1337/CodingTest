a6d431601786f8e0a894d9c16d344e19a2438459
pJona1337 <allsti@wp.pl>
2023-02-11 18:48:22 +0100

    First commit

---
+++ src/main/java/pl/masters/coding/teacher/TeacherController.java
@@ -0,0 +1,20 @@
+package pl.masters.coding.teacher;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/teachers")
+public class TeacherController {
+
+    private final TeacherService teacherService;
+
+    @GetMapping
+    public String getTeacherList() {
+        return "teacher/list";
+    }
+}
